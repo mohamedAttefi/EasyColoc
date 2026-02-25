95cfc2791f917f11e806d3bfbff3f9b0b61ecf41
mariamezraidi <maezraid@gmail.com>
2026-02-25 21:02:21 +0000

    add invitation mail

---
+++ app/Mail/InvitationMail.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Invitation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InvitationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public Invitation $invitation
+    ) {}
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Invitation to join EasyColoc',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.invitation',
+            with: [
+                'invitation' => $this->invitation,
+                'colocation' => $this->invitation->colocation,
+                'inviter' => $this->invitation->inviter,
+                'inviteUrl' => $this->invitation->invite_url,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
